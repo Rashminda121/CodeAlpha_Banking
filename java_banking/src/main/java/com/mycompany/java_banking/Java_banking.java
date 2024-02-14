99ddba2ae8599911b68b1562b0eee563570e2351
Rashminda <rashminda121@gmail.com>
2024-02-14 20:35:18 +0530

    Add files via upload

---
+++ java_banking/src/main/java/com/mycompany/java_banking/Java_banking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_banking;
+
+/**
+ *
+ * @author Rashminda
+ */
+public class Java_banking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
